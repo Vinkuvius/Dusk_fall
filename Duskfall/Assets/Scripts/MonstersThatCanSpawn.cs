edfb0ef271f2dd0280ab36d8b836b82dc44e8739
TtvTemmie <Te21emimal@falufri.se>
2023-10-17 13:18:34 +0200

    stuff

---
+++ Duskfall/Assets/Scripts/MonstersThatCanSpawn.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+
+public class Monster
+{
+    public GameObject monsterPrefab; // The monster's prefab
+    public int maxSpawnCount; // Maximum number of this monster type that can spawn
+}
+
